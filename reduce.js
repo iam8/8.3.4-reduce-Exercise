bc7623737acc5d2853d520173757a399c9b81486
alexm <alex-m@outlook.com>
2022-10-11 16:30:00 -0700

    Broke up long comment lines

--- reduce.js
+++ reduce.js
@@ -20,7 +20,9 @@ function extractValue(arr, key) {
 }
 
 /*
-Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
+Write a function called vowelCount which accepts a string and returns an object with the keys as
+the vowel and the values as the number of times the vowel appears in the string. This function
+should be case insensitive so a lowercase letter and uppercase letter should count
 
 Examples:
     vowelCount('Elie') // {e:2,i:1};
@@ -45,7 +47,8 @@ function vowelCount(str) {
 }
 
 /*
-Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
+Write a function called addKeyAndValue which accepts an array of objects and returns the array of
+objects passed to it with each object now including the key and value passed to the function.
 
 Examples:
     const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
@@ -62,7 +65,11 @@ Examples:
 function addKeyAndValue(arr, key, value) {}
 
 /*
-Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
+Write a function called partition which accepts an array and a callback and returns an array with
+two arrays inside of it. The partition function should run the callback function on each value in
+the array and if the result of the callback function at that specific value is true, the value
+should be placed in the first subarray. If the result of the callback function at that specific
+value is false, the value should be placed in the second subarray.
 
 Examples:
     
